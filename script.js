50aa5313f22aa2205d98b1bc54d36693017f6bb8
Diego <diegoh.bh@gmail.com>
2025-03-29 00:02:24 -0300

    Commit correção calc

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function addNumber(valor){
     const resultField = document.getElementById('result');
     resultField.value += valor;
+    if(resultField.value === 'Erro'){
+        limpar();
+        return;
+    }
 }
 
 function calcular(){
@@ -9,9 +13,16 @@ function calcular(){
         if (resultField.value.trim() === ''){
             return;
         }
+        else if (resultField.value.trim() === 'Erro') {
+            limpar(); 
+            return;  
+        }
         let conta = resultField.value
             .replace(/x/g, '*')
-            .replace(/%/g, '/100');
+            .replace(/(\d+(\.\d+)?)\s*-\s*(\d+(\.\d+)?)%/g, '($1 - ($1 * $3 / 100))')
+            .replace(/(\d+(\.\d+)?)\s*\+\s*(\d+(\.\d+)?)%/g, '($1 + ($1 * $3 / 100))')
+            .replace(/(\d+(\.\d+)?)\s*\/\s*(\d+(\.\d+)?)%/g, '($1 / ($1 * $3 / 100))')
+            .replace(/(\d+(\.\d+)?)\s*\*\s*(\d+(\.\d+)?)%/g, '($1 * ($1 * $3 / 100))');
 
             resultField.value = eval(conta);
     } catch{
